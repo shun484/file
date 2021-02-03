52cd06d4296e2a320fd83fbb7413b990ce8d4e8e
shun484 <shunge321>
2021-02-03 17:00:58 +0800

    提交dev

--- order_server/src/test/java/com/order_server/OrderServerApplicationTests.java
+++ order_server/src/test/java/com/order_server/OrderServerApplicationTests.java
@@ -3,11 +3,15 @@ package com.order_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @SpringBootTest
 class OrderServerApplicationTests {
 
     @Test
     void contextLoads() {
+
+        AtomicInteger integer = new AtomicInteger();
     }
 
 }
